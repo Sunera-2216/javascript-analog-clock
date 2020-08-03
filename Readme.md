547604ed1976d61c906b12e6e61695aad58ab721
Sunera-2216 <sunerasu2216@gmail.com>
2020-08-04 01:12:08 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Javascript Analog Clock
+
+A simple analog clock using HTML, CSS and Javascript.
+Method implementations, styles are explained inside the each file.
---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Calling setClock() and playAudio() in every 1sec.
+setInterval(setClock, 1000);
+setInterval(playAudio, 1000);
+
+const hourIndicator = document.querySelector('[data-hour-indicator]');
+const minuteIndicator = document.querySelector('[data-minute-indicator]');
+const secondIndicator = document.querySelector('[data-second-indicator]');
+const audioEffect = document.getElementById("tickSound");
+
+function setClock() {
+    // Get local time and converting to ratio, which is using as rotation ratio of indicators.
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondIndicator, secondsRatio);
+    setRotation(minuteIndicator, minutesRatio);
+    setRotation(hourIndicator, hoursRatio);
+}
+
+function setRotation(indicator, rotationRatio) {
+    // Set value to --rotation variable in css file.
+    indicator.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+function playAudio() {
+    audioEffect.play();
+}
+
+setClock();
